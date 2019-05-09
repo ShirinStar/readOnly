4f45760313f491dca92fa80d9647cd3b2f302f11
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-05-09 17:21:02 -0400

    listening to title for changes

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,31 @@
 window.onload = function(){
   alert('fuck off')
 
-  const firstTry = document.querySelectorAll('tr');
+  const titleNode = document.querySelector('title');
+  console.log(titleNode);
+  // Options for the observer (which mutations to observe)
+  var config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  var callback = function(mutationsList, observer) {
+      for(var mutation of mutationsList) {
+          if (mutation.type == 'childList') {
+              console.log('A child node has been added or removed.');
+              console.log(titleNode);
+          }
+          else if (mutation.type == 'attributes') {
+              console.log('The ' + mutation.attributeName + ' attribute was modified.');
+          }
+      }
+  };
+
+  // Create an observer instance linked to the callback function
+  var observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(titleNode, config);
+
+  // Later, you can stop observing
 
   console.log(firstTry);
 };

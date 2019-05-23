5d5b2de56ed440bb31e1c0dae799576eeaff63a3
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-05-23 17:27:11 -0400

    bad

--- extension/content.js
+++ extension/content.js
@@ -2,23 +2,33 @@ window.onload = function(){
   alert('fuck off')
 
   const titleNode = document.querySelector('title');
+  const titlesHTML = document.querySelectorAll('span.bqe')
+  const emailTitles = new Set(Array.from(titlesHTML).map(title => title.innerText))
+  console.log(emailTitles);
   console.log(titleNode);
   // Options for the observer (which mutations to observe)
-  cost config = { attributes: true, childList: true, subtree: true };
+  const config = { attributes: true, childList: true, subtree: true };
 
   // Callback function to execute when mutations are observed
-  cost callback = function(mutationsList, observer) {
-      for(let mutation of mutationsList) {
-          if (mutation.type == 'childList') {
-              console.log('A child node has been added or removed.');
-              console.log(titleNode);
+  const callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+      if (mutation.type == 'childList') {
+        emailTitles.forEach(title => {
+          console.log(title);
+          if (titleNode.innerText.includes(title)) {
+            console.log('yo');
+            const body = document.body.innerText.toLowerCase()
+            if (body.includes('unfortunately')) {
+              console.log('we did it');
+            }
           }
-          else if (mutation.type == 'attributes') {
-              console.log('The ' + mutation.attributeName + ' attribute was modified.');
-          }
+        })
+        //console.log(body);
+      } else if (mutation.type == 'attributes') {
+        console.log('The ' + mutation.attributeName + ' attribute was modified.');
       }
-  };
-
+    }
+  }
   // Create an observer instance linked to the callback function
   const observer = new MutationObserver(callback);
 
